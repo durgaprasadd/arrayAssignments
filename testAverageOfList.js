b159dda9e491a7e7352691964101685468bde34e
Durga Prasad <44020610+durgaprasadd@users.noreply.github.com>
2018-10-17 14:35:57 +0530

    this script test the findAverageOfList.js script

--- testAverageOfList.js
+++ testAverageOfList.js
@@ -1,3 +1,3 @@
 let assert = require("assert");
-let findAverageOfList = require("./findAverageOfList.js").sumOfNumbers;
+let findAverageOfList = require("./findAverageOfList.js").findAverageOfList;
 assert.deepEqual(findAverageOfList([1,2,3,4,5]),3);
