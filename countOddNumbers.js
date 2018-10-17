4a1b8478a4b7e297338e34a8eff3433c03a5e388
Durga Prasad <44020610+durgaprasadd@users.noreply.github.com>
2018-10-17 14:57:09 +0530

    this script returns the number of odd numbers in the array

---
+++ countOddNumbers.js
@@ -0,0 +1,11 @@
+const countOddNumbers = function(list){
+  let count=0;
+  for(let index=0;index<list.length;index++){
+    if(list[index]%2 != 0){
+      count++;
+    }
+  }
+  return count;
+}
+
+exports.countOddNumbers = countOddNumbers;
