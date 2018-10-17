bbdd4c9181e5b53cc2eda00058f5135ae319ab1c
Durga Prasad <44020610+durgaprasadd@users.noreply.github.com>
2018-10-17 16:40:38 +0530

    this script checks whether given array is in descending order or not

---
+++ checkDescendingOrder.js
@@ -0,0 +1,12 @@
+const checkDescendingOrder = function(list){
+  let result=true;
+  for(let index=1;index<list.length;index++){
+    if(list[index]>list[index-1]){
+      result = false;
+      break;
+    }
+  }
+  return result;
+}
+
+exports.checkDescendingOrder=checkDescendingOrder;
