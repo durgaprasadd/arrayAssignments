395e62baed1efbea9844f1df93fbba861f62ccca
Durga Prasad <44020610+durgaprasadd@users.noreply.github.com>
2018-10-19 08:58:27 +0530

    this script contains some of the array assignment

--- library.js
+++ library.js
@@ -36,7 +36,66 @@ const intersectionOfArray = function(list1,list2){
   return intersection;
 }
 
+//-------------differenceOfArrays---------
+const differenceOfArrays = function(list1,list2){
+  let difference = [];
+  for(let value of list1){
+    if(!list2.includes(value)){
+      difference.push(value);
+    }
+  }
+  return difference;
+}
+
+//-----------isSubset---------------
+const isSubset = function(list1,list2){
+  let result = true;
+  for(let value of list2){
+    if(!list1.includes(value)){
+      result = false;
+      break;
+    }
+  }
+  return result;
+}
+
+//---------zipArray--------------
+const zipArray = function(list1,list2){
+  let range = Math.min(list1.length,list2.length);
+  let result = [];
+  for(let index=0;index<range;index++){
+    result[index]=[list1[index],list2[index]];
+  }
+  return result;
+}
+
+//-----------rotateArray-------------
+const rotateArray = function(list,count){
+  for(let index=0;index<count;index++){
+    list.push(list.shift());
+  }
+  return list;
+}
+
+//----------partitionOfArray----------
+const partitionOfArray = function(list,limit){
+  let result = [[],[]];
+  for(let value of list){
+    if(value>limit){
+      result[1].push(value);
+    }else{
+      result[0].push(value);
+  }
+  }
+  return result;
+}
+
 exports.extractDigits=extractDigits;
 exports.uniqueOfArray=uniqueOfArray;
 exports.unionOfArray=unionOfArray;
 exports.intersectionOfArray=intersectionOfArray;
+exports.differenceOfArrays=differenceOfArrays;
+exports.isSubset = isSubset;
+exports.zipArray = zipArray;
+exports.rotateArray = rotateArray;
+exports.partitionOfArray = partitionOfArray;
