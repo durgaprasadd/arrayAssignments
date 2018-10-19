a11404e4e4c2aa4223dab03fc606348098e945d1
Durga Prasad <44020610+durgaprasadd@users.noreply.github.com>
2018-10-19 08:58:43 +0530

    this script test the library.js script

--- testLibrary.js
+++ testLibrary.js
@@ -4,6 +4,11 @@ let extractDigits = lib.extractDigits;
 let uniqueOfArray = lib.uniqueOfArray;
 let unionOfArray = lib.unionOfArray;
 let intersectionOfArray = lib.intersectionOfArray;
+let differenceOfArrays = lib.differenceOfArrays;
+let isSubset = lib.isSubset;
+let zipArray = lib.zipArray;
+let rotateArray = lib.rotateArray;
+let partitionOfArray = lib.partitionOfArray;
 
 //-------extract--Digits-----------
 assert.deepEqual(extractDigits(12),[1,2]);
@@ -29,3 +34,31 @@ assert.deepEqual(intersectionOfArray([1],[]),[]);
 assert.deepEqual(intersectionOfArray([],[1]),[]);
 assert.deepEqual(intersectionOfArray([1],[1]),[1]);
 
+//-------------differenceOfArrays---------
+assert.deepEqual(differenceOfArrays([],[]),[]);
+assert.deepEqual(differenceOfArrays([1],[]),[1]);
+assert.deepEqual(differenceOfArrays([],[1]),[]);
+assert.deepEqual(differenceOfArrays([1,2,3,4,5],[4,2,1]),[3,5]);
+
+//-----------isSubset---------------
+assert.deepEqual(isSubset([],[]),true);
+assert.deepEqual(isSubset([1],[]),true);
+assert.deepEqual(isSubset([],[1]),false);
+assert.deepEqual(isSubset([1,2,3,4,5],[3,2]),true);
+
+//---------zipArray--------------
+assert.deepEqual(zipArray([1],[]),[]);
+assert.deepEqual(zipArray([1],[1]),[[1,1]]);
+assert.deepEqual(zipArray([1,2],[1]),[[1,1]]);
+assert.deepEqual(zipArray([1,2,3,4,5],[3,2]),[[1,3],[2,2]]);
+
+//-----------rotateArray-------------
+assert.deepEqual(rotateArray([1,2,3],0),[1,2,3]);
+assert.deepEqual(rotateArray([1,2,3],1),[2,3,1]);
+assert.deepEqual(rotateArray([1,2,3],2),[3,1,2]);
+assert.deepEqual(rotateArray([1,2,3],3),[1,2,3]);
+
+//----------partitionOfArray----------
+assert.deepEqual(partitionOfArray([1,2,3],0),[[],[1,2,3]]);
+assert.deepEqual(partitionOfArray([1,2,3],3),[[1,2,3],[]]);
+assert.deepEqual(partitionOfArray([1,2,3,4,5],2),[[1,2],[3,4,5]]);
